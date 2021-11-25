819b8090af28641f67c427673b8b0792a17fa9e6
Dmitriy <dmitriy.botsman@gmail.com>
2021-11-25 02:45:12 +0200

    instal simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
-import notiflix from 'notiflix';
-import searchImages from './api-servis';
+import Notiflix from 'notiflix';
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const formRef = document.querySelector('#search-form');
 const inputRef = document.querySelector('.page-head__input');
@@ -9,18 +12,53 @@ const galleryRef = document.querySelector('.gallery');
 const loadMoreRef = document.querySelector('.load-more');
 
 const myKey = '24498765-29ee438a61ceedd9aaf6213cc';
+let currentName = '';
+let page = 1;
+const BASE_URL = `https://pixabay.com/api/`;
 
-const BASE_URL = `https://pixabay.com/api/?key=${myKey}&q=${currentName}&image_type=photo&orientation=horizontal&safesearch=true&page=${pages}&per_page=40`;
-
-const searchImages = rrr => {
-  const rrr = dog;
-  return fetch(`${BASE_URL}`).then(res => res.json());
+const searchImages = currentName => {
+  return fetch(
+    `${BASE_URL}?key=${myKey}&q=${currentName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`,
+  )
+    .then(res => res.json())
+    .then(data => data);
 };
 
 formRef.addEventListener('submit', e => {
-  e.preventDefault(searchParams);
-  searchImages().then(rrr => {
+  e.preventDefault();
+  page = 1;
+
+  currentName = e.currentTarget.elements.searchQuery.value;
+  searchImages(currentName).then(rrr => {
+    loadMoreRef.style.display = 'none';
     galleryRef.innerHTML = marcapGallery(rrr.hits);
+    if (rrr.total === 0) {
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+      );
+    } else {
+      return (
+        Notiflix.Notify.success(`'Hooray! We found ${rrr.totalHits} images.'`),
+        (loadMoreRef.style.display = 'block')
+      );
+    }
+
+    console.log(rrr);
+    page += 1;
+  });
+});
+
+loadMoreRef.addEventListener('click', e => {
+  searchImages(currentName).then(rrr => {
+    if (page <= Math.floor(rrr.totalHits % 40)) {
+      return (
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results."),
+        (loadMoreRef.style.display = 'none')
+      );
+    }
+    page += 1;
+    console.log(page);
+    galleryRef.insertAdjacentHTML('beforeend', marcapGallery(rrr.hits));
   });
 });
 
